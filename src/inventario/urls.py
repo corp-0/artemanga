5f22741bc2549edd74e563b14343a6cfa0a988bd
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-04-12 18:38:07 -0400

    refactor: simplificada declaración de urls que no usan regex

--- src/inventario/urls.py
+++ src/inventario/urls.py
@@ -1,13 +1,23 @@
-from django.urls import re_path
-from inventario.views import AutorListView, GeneroListView, PaisListView, EditorialListView, OtrosAutoresListView, \
-    IVAListView, ProductoListView
+from django.urls import path
+from inventario.views import (
+    AutorListView, GeneroListView, PaisListView, EditorialListView,
+    OtrosAutoresListView, IVAListView, ProductoListView, ProductoCreateView,
+    ProductoUpdateView, ProductoDeleteView
+)
 
 urlpatterns = [
-    re_path('listado-producto', ProductoListView.as_view()),
-    re_path('listado-autor', AutorListView.as_view()),
-    re_path('listado-genero', GeneroListView.as_view()),
-    re_path('listado-pais', PaisListView.as_view()),
-    re_path('listado-editorial', EditorialListView.as_view()),
-    re_path('listado-otro-autor', OtrosAutoresListView.as_view()),
-    re_path('listado-iva', IVAListView.as_view()),
+    # listados
+    path('listado-producto', ProductoListView.as_view(), name='listado-producto'),
+    path('listado-autor', AutorListView.as_view(), name='listado-autor'),
+    path('listado-genero', GeneroListView.as_view(), name='listado-genero'),
+    path('listado-pais', PaisListView.as_view(), name='listado-pais'),
+    path('listado-editorial', EditorialListView.as_view(), name='listado-editorial'),
+    path('listado-otro-autor', OtrosAutoresListView.as_view(), name='listado-otro-autor'),
+    path('listado-iva', IVAListView.as_view(), name='listado-iva'),
+    # crear
+    path('crear-producto', ProductoCreateView.as_view(), name='crear-producto'),
+    # editar
+    path('editar-producto/<pk>/', ProductoUpdateView.as_view(), name='editar-producto'),
+    # eliminar
+    path('eliminar-producto/<pk>/', ProductoDeleteView.as_view(), name='eliminar-producto'),
 ]
