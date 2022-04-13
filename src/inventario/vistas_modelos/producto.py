b21078f04f574051a5f6cc7a0275b97695c4c19b
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-04-12 21:07:46 -0400

    refactor: separa vistas en sus propios módulos

--- src/inventario/vistas_modelos/producto.py
+++ src/inventario/vistas_modelos/producto.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+from django.views.generic import ListView
 
 from inventario.models import Producto
+from .vistas_genericas import CrearGenerico, ActualizarGenerico, EliminarGenerico
 
 
 class ProductoListView(ListView):
@@ -13,7 +14,6 @@ class ProductoListView(ListView):
         context = super().get_context_data(**kwargs)
         contexto_extra = {
             'url_crear': 'crear-producto',
-            'boton_crear': 'Crear producto',
             'url_editar': 'editar-producto',
             'url_eliminar': 'eliminar-producto',
         }
@@ -22,21 +22,18 @@ class ProductoListView(ListView):
         return context
 
 
-class ProductoUpdateView(UpdateView):
+class ProductoUpdateView(ActualizarGenerico):
     model = Producto
-    template_name = 'CRUD/form_generico.html'
+    success_url = reverse_lazy('listado-producto')
     fields = '__all__'
-    success_url = reverse_lazy('listado-productos')
 
 
-class ProductoCreateView(CreateView):
+class ProductoCreateView(CrearGenerico):
     model = Producto
-    template_name = 'CRUD/form_generico.html'
+    success_url = reverse_lazy('listado-producto')
     fields = '__all__'
-    success_url = reverse_lazy('listado-productos')
 
 
-class ProductoDeleteView(DeleteView):
+class ProductoDeleteView(EliminarGenerico):
     model = Producto
-    template_name = 'CRUD/eliminar_generico.html'
-    success_url = reverse_lazy('listado-productos')
+    success_url = reverse_lazy('listado-producto')
