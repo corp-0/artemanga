dcb84b4ebffc419d61e009185f1be053edb5b8e5
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-04-28 16:39:45 -0400

    chore: corrige conflicto

--- src/catalogo/views.py
+++ src/catalogo/views.py
@@ -6,10 +6,10 @@ from inventario.models import Genero, Editorial
 class Home(TemplateView):
     template_name= "web/base.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(Home, self).get_context_data(**kwargs)
-        context['categorias'] = Genero.objects.all()
-        context['editoriales'] = Editorial.objects.all()
-
-        return context
-        
+    # def get_context_data(self, **kwargs):
+    #     context = super(Home, self).get_context_data(**kwargs)
+    #     context['categorias'] = Genero.objects.all()
+    #     context['editoriales'] = Editorial.objects.all()
+    #
+    #     return context
+    #
