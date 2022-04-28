2f5b560d208969977fa14a35177719f594f2d509
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-04-28 16:46:11 -0400

    feat: agrega editoriales y categorias a contexto común para fácil acceso

--- src/artemangaweb/settings.py
+++ src/artemangaweb/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'artemangaweb.contexto_comun.obtener_editoriales_y_categorias',
             ],
         },
     },
--- src/catalogo/views.py
+++ src/catalogo/views.py
@@ -1,15 +1,6 @@
 from django.views.generic import TemplateView
-from inventario.models import Genero, Editorial
 
 
 # Create your views here.
 class Home(TemplateView):
     template_name= "web/base.html"
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(Home, self).get_context_data(**kwargs)
-    #     context['categorias'] = Genero.objects.all()
-    #     context['editoriales'] = Editorial.objects.all()
-    #
-    #     return context
-    #
