65acaa628de4c1cc46f99c6bf063d85289f5bffd
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-05-04 01:21:19 -0400

    refactor: eliminado módulo despacho y combinado con ventas
    BREAKING CHANGE: es necesario destruir y recrear la base de datos para que siga funcionando el proyecto.

--- src/venta/enums/opciones.py
+++ src/venta/enums/opciones.py
@@ -11,3 +11,13 @@ class EstadoVenta(Enum):
 
 
 ESTADO_VENTA_CHOICES = [[estadoVenta.value, estadoVenta.name.capitalize()] for estadoVenta in EstadoVenta]
+
+
+class EstadoDes(Enum):
+    PENDIENTE = 1
+    EN_PROCESO = 2
+    FINALIZADO = 3
+    FALLIDO = 4
+
+
+ESTADO_DESPACHO_CHOICE = [[estadoDes.value, estadoDes.name.capitalize()] for estadoDes in EstadoDes]
