31fc8ce438b159cb9ca2f74635ac528d5ee75a91
allanhack1 <82013172+allanhack1@users.noreply.github.com>
2022-05-06 19:02:51 -0400

    Detalle producto version1.1

--- src/catalogo/urls.py
+++ src/catalogo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, VerCarritoView, ActualizarCarritoView, AgregarProductoCarritoView, DetalleProducto,
+from .views import Home, VerCarritoView, ActualizarCarritoView, AgregarProductoCarritoView, DetalleProducto
 
 
 urlpatterns = [
